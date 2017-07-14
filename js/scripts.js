06e2a68771bdc531febaeefbbf034de5d0ea85f9
Carson Canales <cc020889@icloud.com>
2017-07-14 16:16:04 -0700

    add comments in js for future reference

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
 // user interfsce logic
+
+
+// loads document
 $(document).ready(function(){
+  // gets imput from form under the footer id and appends variable umber to the  class user1
   $("#footer form").submit(function(event) {
-    var number = $("input#input1").val();
     $(".user1").append("<li>"+number+"</li>"+"<br>");
+    // assigns the variable number = to the value of the imput of the form
+      var number = $("input#input1").val();
+    // prevents the document from refreshing
     event.preventDefault();
     var userinput = "#"
     $(".user1").show();
