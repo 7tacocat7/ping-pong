8eb264e0e90e7d698d21de2a732602317e132f74
Carson Canales <cc020889@icloud.com>
2017-07-14 16:07:12 -0700

    add first spec

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@
 $(document).ready(function(){
   $("#footer form").submit(function(event) {
     var number = $("input#input1").val();
-    $(".user1").append("<ul><li>"+number+"</li></ul>"+"<br>");
+    $(".user1").append("<li>"+number+"</li>"+"<br>");
     event.preventDefault();
     var userinput = "#"
-
+    $(".user1").show();
   });
 });
