d16382bb364ac6183aba35fe73f96f7c30d81396
Carson Canales <cc020889@icloud.com>
2017-07-21 02:57:33 -0700

    add li click to remove functionality in js

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,25 @@ for (i = 0; i <=number;i++) {
 
 
 // user interfsce logic
+var blankArray =[] ;
 
 
 // loads document
 $(document).ready(function(){
-  // gets input from form under the footer id and appends variable umber to the  class user1
   $("#footer form").submit(function(event) {
+
+
+    // user interfsce logic
     var number = $("input#input1").val();
     $(".user1").append("<li>"+number+"</li>"+"<br>");
+    $("span.user1").children("li").click(function() {
+      $(this).remove();
+    });
     // assigns the variable number = to the value of the imput of the form
       // var number = $("input#input1").val();
       // var rangeinput = .parsInt(number);
     // prevents the document from refreshing
+    var numberArray = blankArray.push(number);
     event.preventDefault();
     var userinput = "#"
     $(".user1").show();
