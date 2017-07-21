1bff5bb9d7aecf8e277ceeec78649ae2c0cd821d
Carson Canales <cc020889@icloud.com>
2017-07-21 03:41:26 -0700

    add alerts in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,26 @@
-// Business logic
-var numberarray =[]
-for (i = 0; i <=number;i++) {
-  numberarray.push(i);
-}
+//
+// var number = $("input#input1").val();
+// buisness logic
+var numberArray =[]
 
-
-// user interfsce logic
-var blankArray =[] ;
 
 
 // loads document
 $(document).ready(function(){
   $("#footer form").submit(function(event) {
 
+    alert(" thanks for submitting your number")
 
     // user interfsce logic
-    var number = $("input#input1").val();
-    $(".user1").append("<li>"+number+"</li>"+"<br>");
+    var userInput = $("input#input1").val();
+    $(".user1").append("<li>"+userInput+"</li>"+"<br>");
     $("span.user1").children("li").click(function() {
       $(this).remove();
+      alert(" look! you removed a number")
     });
-    // assigns the variable number = to the value of the imput of the form
-      // var number = $("input#input1").val();
-      // var rangeinput = .parsInt(number);
-    // prevents the document from refreshing
-    var numberArray = blankArray.push(number);
-    event.preventDefault();
-    var userinput = "#"
+
     $(".user1").show();
+      alert(" you got to the end");
+      event.preventDefault();
   });
 });
