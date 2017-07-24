2badc9488131c67df7732e5c8a50cc99a6288d58
Carson Canales <cc020889@icloud.com>
2017-07-23 22:21:42 -0700

    add notes to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,125 +1,64 @@
-//
-// var number = userInput.parsInt();
-// buisness logic
-// var userInput = $("input#input1").val();
-// var numericInput = parseInt(userInput);
-// for (var index = 0; index <= userInput; index += 1) {
-//     this.push(numberArray);
-// }
-
-// var countToInputNumber = function(count) {
-//   var numberArray = [];
-//   var number = parseInt($("input#input1").val());
-//     for (var index = 0; index<= count; index ++){
-//       if(index%3 === 0){
-//         countToInputNumber.push("ping");
-//       }
-//       else if (index%5 ===0) {
-//         countToInputNumber.push("pong");
-//       }
-//       else if (index%15 ===0){
-//         countToInputNumber.push("pingpong");
-//       }
-//       else{
-//         numberArray.push(index);
-//       }
-//     }
-//     return numberArray;
-// };
-// var inputnumber = parseInt($("input#input1").val());
-// var pingpongCount = function(count){
-// var numbers = [];
-//
-//   for (var i =1; i<= count; i ++){
-//     numbers.push(i);
-//     return numbers;
-//   };
-// };
 
 
 
-
-// var start = 0;
-// var inputnumber = parseInt($("input#input1").val());
-// var numbers = [];
-// for (var i = start; i < inputnumber+1; i++) {
-//     numbers.push(i);
-// }
-//
-// var pingpong = numbers.map(function(number) {
-//   if (number === 0){
-//       return number;
-//    }
-//       else if (number%5 === 0 && number%15 === 0){
-//       return "ping-pong";
-//       }
-//    else if (number%5 === 0){
-//       return "pong";
-//       }
-//       else if (number%3 === 0){
-//          return "ping";
-//          }
-//    else {
-//    return number;
-//       }
-// });
-
-
-
-
-
-
-
-
-
-  // user interfsce logic
-// loads document
 
 $(document).ready(function(){
   $("#footer form").submit(function(event) {
+
+
+//buisness logic
+var start = 0;
+//start of the count^
+var inputnumber = parseInt($("input#input1").val());
+//defines the variable inputnumber for use in buisness logic
+var numbers = [];
+//creates empty array to store the number count^
+for (var i = start; i < inputnumber+1; i++) {
+    numbers.push(i);
+}
+// counts up to the input number and puses values to array^
+var pingpong = numbers.map(function(number) {
+  if (number === 0){
+      return number;
+   }
+      else if (number%5 === 0 && number%15 === 0){
+      return "ping-pong";
+      }
+   else if (number%5 === 0){
+      return "pong";
+      }
+      else if (number%3 === 0){
+         return "ping";
+         }
+   else {
+   return number;
+      }
+});
+//creates a loop from that replaces values (divisible by 3,5,15) with the strings 'ping' 'pong' and 'pingpong' and places values in a new array called pingpong^
+
+
+// end of buisness logic
   var userInput = $("input#input1").val();
-    alert(userInput);
+  // takes input val from ui
+    // alert(userInput);
     // alert(pingpong);
-    var start = 0;
-    var inputnumber = parseInt($("input#input1").val());
-    var numbers = [];
-    for (var i = start; i < inputnumber+1; i++) {
-        numbers.push(i);
-    }
-
-    var pingpong = numbers.map(function(number) {
-      if (number === 0){
-          return number;
-       }
-          else if (number%5 === 0 && number%15 === 0){
-          return "ping-pong";
-          }
-       else if (number%5 === 0){
-          return "pong";
-          }
-          else if (number%3 === 0){
-             return "ping";
-             }
-       else {
-       return number;
-          }
-    });
-
 
   var number = parseInt($("input#input1").val());
       if (isNaN(number)){
         alert("Please enter a number value!");
       }
+      //alerts user if input is not a number
   pingpong.forEach(function(number){
     $(".user1").append("<li>"+number+"</li>"+"<br>");
-
   })
+//appends each element of the array pingpong to a list within the page and seperates each item with a page break
 
-    // removes appended list items when clicked
     $("span.user1").children("li").click(function() {
       $(this).remove();
     });
+    // removes appended list items when clicked^
     $(".user1").show();
+    //shows list items after submit button has been pushed^
     event.preventDefault();
   });
 
