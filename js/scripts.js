f846a70f7e64cc4750cc9ef21c2f8682ad286ddb
Carson Canales <cc020889@icloud.com>
2017-07-23 21:03:35 -0700

    add map loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,67 @@
 //
 // var number = userInput.parsInt();
 // buisness logic
-// var numberArray =[]
-// var userInput = parseInt($("input#input1").val());
+// var userInput = $("input#input1").val();
+// var numericInput = parseInt(userInput);
 // for (var index = 0; index <= userInput; index += 1) {
 //     this.push(numberArray);
 // }
 
-var countToInputNumber = function(count) {
-  var numberArray = [];
-    for (var index = 0; i<= count; i ++){
-      if(index%3 === 0){
-        countToInputNumber.push("ping");
-      }
-      else if (index%5 ===0) {
-        countToInputNumber.push("pong")
-      }
-    }
+// var countToInputNumber = function(count) {
+//   var numberArray = [];
+//   var number = parseInt($("input#input1").val());
+//     for (var index = 0; index<= count; index ++){
+//       if(index%3 === 0){
+//         countToInputNumber.push("ping");
+//       }
+//       else if (index%5 ===0) {
+//         countToInputNumber.push("pong");
+//       }
+//       else if (index%15 ===0){
+//         countToInputNumber.push("pingpong");
+//       }
+//       else{
+//         numberArray.push(index);
+//       }
+//     }
+//     return numberArray;
+// };
+// var inputnumber = parseInt($("input#input1").val());
+// var pingpongCount = function(count){
+// var numbers = [];
+//
+//   for (var i =1; i<= count; i ++){
+//     numbers.push(i);
+//     return numbers;
+//   };
+// };
+
+
+
+
+var start = 0;
+var inputnumber = parseInt($("input#input1").val());
+var numbers = [];
+for (var i = start; i < inputnumber+1; i++) {
+    numbers.push(1);
 }
+
+var pingpong = numbers.map(function(number) {
+  if (number%5 === 0 && number%15 === 0){
+      return "ping-pong";
+   }
+      else if (number%5 === 0){
+      return "pong";
+      }
+   else if (number%3 === 0){
+      return "ping";
+      }
+   else {
+   return number;
+      }
+});
+
+
 
 
 
@@ -30,9 +74,22 @@ var countToInputNumber = function(count) {
 
 $(document).ready(function(){
   $("#footer form").submit(function(event) {
-    var userInput = $("input#input1").val();
-    $(".user1").append("<li>"+userInput+"</li>"+"<br>");
-  forEach
+  var userInput = $("input#input1").val();
+    alert(userInput)
+
+  var number = parseInt($("input#input1").val());
+      if (isNaN(number)){
+        alert("Please enter a number value!");
+
+      }
+      else{
+        var outcome = counters(number);
+      }
+      counters.forEach(function(number){
+        $(".user1").append("<li>"+number+"</li>"+"<br>");
+
+      })
+
     // removes appended list items when clicked
     $("span.user1").children("li").click(function() {
       $(this).remove();
